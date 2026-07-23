acfabd495faae83e7536059c23ed89817fccdbe3
KacperZa <lacper.zajac765@gmail.com>
2026-07-23 21:02:26 +0200

    feat: Dodano nowe sekcje

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
+import AboutUs from './components/AboutUs'
+import Instagram from './components/Instagram'
 import Introduction from './components/Introduction'
 import Navbar from './components/Navbar'
 import Offer from './components/Offer'
+import Projects from './components/Projects'
 
 function App() {
 
   return (
     <>
-    <div className='min-w-screen h-screen overflow-x-hidden'>
+    <div className='min-w-screen h-screen overflow-x-hidden relative'>
       <Navbar />
       <Introduction />
       <Offer />
+      <AboutUs />
+      <Projects />
+      <Instagram />
     </div>
     </>
   )
