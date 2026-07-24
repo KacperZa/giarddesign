04203e8d3c039e0a028cd89c50b324808046e4e3
KacperZa <lacper.zajac765@gmail.com>
2026-07-24 21:09:54 +0200

    style: Poprawiono czcionki, wysokości linii, odstępy między znakami

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -22,18 +22,18 @@ const AboutUs = () => {
                 {/* Pojemnik na tekst */}
                 <div className="grid grid-cols-5 gap-11 w-full text-white">
                     <div className='col-span-4 grid grid-cols-4 gap-x-16 gap-4'>
-                        <p className='text-grey text-xs font-inter'>O firmie</p>
+                        <p className='text-grey text-xs font-inter leading-[150%] tracking-[-1%]'>O firmie</p>
                         <div className='flex flex-col col-span-4'>
                             <div className="grid grid-cols-4 gap-x-16 gap-10">
-                                <p className="font-montserrat font-medium text-5xl col-span-3 leading-[115%] ">Tworzymy z  <span className='italic font-inter font-medium'>pasją</span></p>
-                                <p className="font-inter col-span-4 text-pretty leading-[150%]  font-normal">Każdy projekt to nowe wyzwanie. Dlatego nasz zespół tworzą wykwalifikowani projektanci oraz architekci, których zadaniem jest rozpoznanie i realizacja potrzeb każdego Klienta. Nasza specjalizacja to przestrzenie nowoczesne, które charakteryzuje minimalizm, geometria i elegancka prostota. Tworzymy ogrody małoobsługowe, dostosowane do współczesnego trybu życia.</p>
+                                <p className="font-montserrat font-medium text-5xl col-span-3 leading-[115%] tracking-[-3%] ">Tworzymy z  <span className='italic font-inter font-medium'>pasją</span></p>
+                                <p className="font-inter col-span-4 text-pretty leading-[150%] font-normal">Każdy projekt to nowe wyzwanie. Dlatego nasz zespół tworzą wykwalifikowani projektanci oraz architekci, których zadaniem jest rozpoznanie i realizacja potrzeb każdego Klienta. Nasza specjalizacja to przestrzenie nowoczesne, które charakteryzuje minimalizm, geometria i elegancka prostota. Tworzymy ogrody małoobsługowe, dostosowane do współczesnego trybu życia.</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* Pojemnik na przyciski  */}
                 <div>
-                <button className="border-grey pl-5 pr-5 pt-3 pb-3.5 border rounded-full text-grey font-inter font-normal leading-normal flex gap-1 items-center">Zobacz nasze realizacje  <ArrowRight /> </button>
+                <button className="border-grey pl-5 pr-5 pt-3 pb-3.5 border rounded-full text-grey font-inter font-normal leading-[150%] flex gap-1 items-center">Poznaj nas bliżej  <ArrowRight /> </button>
                 </div>
 
               </div>
--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -4,34 +4,34 @@ import { motion } from "motion/react"
 const Offer = () => {
   return (
     <>
-        <div className='h-full w-full bg-grey grid grid-cols-12 gap-x-16  items-center'>
+        <div className='h-9/10 w-full bg-grey grid grid-cols-12 gap-x-16  items-center'>
             {/* Pojemnik na tekst  */}
-            <div className='col-span-8 flex flex-col col-start-2 '>
+            <div className='col-span-8 flex col-start-2 '>
                 <div className="grid grid-cols-10 gap-x-16 gap-4">
-                    <div className=" col-span-10 font-inter text-xs text-green">Oferta</div>
+                    <div className=" col-span-10 font-inter text-xs text-green leading-[150%] tracking-[-1%]">Oferta</div>
                     <div className="flex flex-col gap-10 col-span-7">
-                        <div className="flex flex-row text-5xl gap-1"> 
-                            <div className="font-montserrat font-medium">Działamy</div>  
-                            <div className="font-inter font-medium italic">kompleksowo</div>
+                        <div className=" text-5xl  leading-[115%] font-medium"> 
+                            <span className="font-montserrat tracking-[-3%]">Działamy </span>  
+                            <span className="font-inter italic ">kompleksowo</span>
                         </div>
-                        <p className="text-left">Oferujemy kompletną obsługę inwestycji terenów zielonych. Projektujemy nowoczesne ogrody przydomowe oraz rezydencjonalne. Stworzymy dla Ciebie projekt, zwizualizujemy go i wcielimy w życie, a na każdym etapie posłużymy radą i wieloletnim doświadczeniem. </p>
+                        <p className="text-left font-inter leading-[150%]">Oferujemy kompletną obsługę inwestycji terenów zielonych. Projektujemy nowoczesne ogrody przydomowe oraz rezydencjonalne. Stworzymy dla Ciebie projekt, zwizualizujemy go i wcielimy w życie, a na każdym etapie posłużymy radą i wieloletnim doświadczeniem. </p>
                     </div>
                 </div>
             </div>
 
             {/* Pojemnik na karty  */}
-            <div className="col-span-12 flex flex-row px-22 gap-16 justify-between">
+            <div className="col-span-12 flex flex-row px-22 gap-16 gap-x-16 justify-between">
 
                 <motion.div className="bg-white px-10 py-12 h-92.5 w-94.5 rounded-3xl flex flex-col justify-between cursor-pointer group"
                 whileHover={{scale: 1.03}}>
                     <div className="flex flex-col gap-8">
                         <PencilSparkles size={35} rotate={18} />
                         <div className="flex flex-col gap-3">
-                            <p className="font-montserrat text-2xl leading-[115%]">Projekty</p>
+                            <p className="font-montserrat text-2-5xl leading-[115%]">Projekty</p>
                             <p className="font-inter text-sm leading-[150%]">Zaprojektujemy Twój ogród w nowoczesnym stylu i z najlepszym wykorzystaniem istniejącej przestrzeni.</p>
                         </div>
                     </div>
-                    <p className="flex flex-row text-green under underline-animate"> Dowiedz się więcej <ChevronRight /></p>
+                    <p className="flex flex-row text-green under underline-animate font-inter leading-[150%]"> Dowiedz się więcej <ChevronRight /></p>
                 </motion.div>
 
                 <motion.div className="bg-white px-10 py-12 h-92.5 w-94.5 rounded-3xl flex flex-col justify-between cursor-pointer group"
@@ -39,11 +39,11 @@ const Offer = () => {
                     <div className="flex flex-col gap-8">
                         <Eye size={35} rotate={18} />
                         <div className="flex flex-col gap-3">
-                            <p className="font-montserrat text-2xl leading-[115%]">Wizualizacje</p>
+                            <p className="font-montserrat text-2-5xl leading-[115%]">Wizualizacje</p>
                             <p className="font-inter text-sm leading-[150%]">Przedstawimy Ci projekty koncepcyjne w postaci wirtualnego spaceru animowanego w technologii 3D.</p>
                         </div>
                     </div>
-                    <p className="flex flex-row text-green under underline-animate"> Dowiedz się więcej <ChevronRight /></p>
+                    <p className="flex flex-row text-green under underline-animate font-inter leading-[150%]"> Dowiedz się więcej <ChevronRight /></p>
                 </motion.div>
 
                 <motion.div className="bg-white px-10 py-12 h-92.5 w-94.5 rounded-3xl flex flex-col justify-between cursor-pointer group"
@@ -51,11 +51,11 @@ const Offer = () => {
                     <div className="flex flex-col gap-8">
                         <Sparkles size={35} rotate={18} />
                         <div className="flex flex-col gap-3">
-                            <p className="font-montserrat text-2xl leading-[115%]">Projekty</p>
-                            <p className="font-inter text-sm leading-[150%]">Zaprojektujemy Twój ogród w nowoczesnym stylu i z najlepszym wykorzystaniem istniejącej przestrzeni.</p>
+                            <p className="font-montserrat text-2-5xl leading-[115%]">Projekty</p>
+                            <p className="font-inter text-sm leading-[150%]">Zrealizujemy Twoje marzenie przy użyciu najnowszych rozwiązań i zaawansowanych technologii.</p>
                         </div>
                     </div>
-                    <p className="flex flex-row text-green under underline-animate"> Dowiedz się więcej <ChevronRight /></p>
+                    <p className="flex flex-row text-green under underline-animate font-inter leading-[150%]"> Dowiedz się więcej <ChevronRight /></p>
                 </motion.div>
             </div>
         </div>
