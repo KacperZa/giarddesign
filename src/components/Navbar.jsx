d3b332f3b93230b089a206b2d7546e685af590ab
KacperZa <lacper.zajac765@gmail.com>
2026-07-24 21:13:01 +0200

    feat: Dodano wysuwającą się wyszukiwarkę oraz rozwijany dropdown Oferty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,141 @@
 import Logo from "../ui/svg/Logo"
 import { Search, ChevronDown } from "lucide-react"
-import { motion } from "motion/react"
-import { useState } from "react"
+import { motion, AnimatePresence } from "motion/react"
+import { useEffect, useRef, useState } from "react"
+
+// Warianty dla strzałki przy "Oferta"
+const arrowVariants = {
+    rest: { rotate: 0},
+    hover: { rotate: 180},
+};
+
+// Warianty dla containera dropdowna
+const dropdownContainerVariants = {
+    hidden: {
+        height: 0
+    },
+    show: {
+        height: 'auto',
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+    exit: {
+        height: 0,
+        transition: {
+            duration: 0.6,
+            staggerChildren: 0.1,
+            staggerDirection: -1
+        }
+    },
+};
+
+// Warianty dla div-ow w srodku dropdownu
+const dropdownItemVariants = {
+    hidden: {
+        opacity: 0,
+        y: -10
+    },
+    show: {
+        opacity: 1,
+        y: 0
+    },
+    exit: {
+        opacity: 0,
+        y: -10,
+        transition: {
+            duration: 0.1
+        }
+    }
+};
+
 
 const Navbar = () => {
     const [searchBarVisible, setSearchBarVisible] = useState(false)
+    const [isHovered, setIsHovered] = useState(false)
+    const [isPinned, setIsPinned] = useState(false)
+
+    const isDropdownVisible = isPinned || isHovered
+
+    const searchBarRef = useRef(null)
+    const dropdownRef = useRef(null)
+
+    useEffect(() => {
+        const handler = (e) => {
+            if(!searchBarRef.current?.contains(e.target)) {
+                setSearchBarVisible(false)
+            }
+        }
+        window.addEventListener('click', handler)
+        return () => window.removeEventListener('click', handler)
+    }, [])
+
+    useEffect(() => {
+        const handler = (e) => {
+            if(!dropdownRef.current?.contains(e.target)) {
+                setIsPinned(false)
+            }
+        }
+        window.addEventListener('click', handler)
+        return () => window.removeEventListener('click', handler)
+    }, [])
+
   return (
     <>
-        <nav className="w-full h-1/10 px-22 ">
-            <div className="flex items-center justify-between w-full h-full">
+        <nav className="w-full h-1/10 px-22 sticky top-0 z-50 bg-white ">
+            <div className="flex items-center justify-between w-full h-full relative">
                 <p className="flex items-center"> <Logo /></p>
-                <div className="flex flex-row gap-12 font-inter"> 
-                    <div className={`flex flex-row items-center gap-1 cursor-pointer tracking-wide ${searchBarVisible ? 'hidden' : null}`}>
+                <div ref={searchBarRef} className={`flex flex-row  gap-12 font-inter  leading-[150%] tracking-[-1%]  justify-center items-center`}> 
+                    
+                    <motion.div 
+                    className={`flex flex-row items-center gap-1  ${searchBarVisible ? 'opacity-0 pointer-events-none' : 'cursor-pointer'} transition-opacity duration-200 ease-in-out relative`}
+                    initial="rest"
+                    whileHover="hover"
+                    onClick={() => setIsPinned(true)}
+                    onHoverStart={() => setIsHovered(true)}
+                    onHoverEnd={() => setIsHovered(false)}
+                    ref={dropdownRef}
+                        >
                         Oferta 
                         <motion.div 
-                        whileHover={{rotate: 180}}
+                        variants={arrowVariants}
                         transition={{
                             damping:20
                         }}
                         >
                             <ChevronDown size={20}/>
                         </motion.div>
-                    </div>
-                    <div className={`tracking-wide ${searchBarVisible ? 'hidden' : null}`}>O firmie</div>
-                    <div className={`tracking-wide ${searchBarVisible ? 'hidden' : null}`}>Realizacje</div>
-                    <div className={`tracking-wide ${searchBarVisible ? 'hidden' : null}`}>Kontakt</div>
-                    <div onClick={() => setSearchBarVisible(prev => !prev)}><Search /></div>
-                    {searchBarVisible && 
-                    <>
-                    <input /> dasd
-                    </>
-                    }
+                        <AnimatePresence>
+                            {isDropdownVisible && 
+                                <motion.div className="absolute top-full left-1/2 -translate-x-1/2  flex flex-col gap-4 bg-white p-2 rounded-xl"
+                                variants={dropdownContainerVariants}
+                                initial='hidden'
+                                animate='show'
+                                exit='exit'
+                                >
+                                    <motion.div variants={dropdownItemVariants} className=" rounded-xl pl-6 pr-6 pt-3 pb-3.5 bg-grey">Projekty</motion.div>
+                                    <motion.div variants={dropdownItemVariants} className=" rounded-xl pl-6 pr-6 pt-3 pb-3.5 bg-grey">Oferta</motion.div>
+                                    <motion.div variants={dropdownItemVariants} className=" rounded-xl pl-6 pr-6 pt-3 pb-3.5 bg-grey">O firmie</motion.div>
+                                    <motion.div variants={dropdownItemVariants} className=" rounded-xl pl-6 pr-6 pt-3 pb-3.5 bg-grey">Nasze projekty</motion.div>
+                                </motion.div>
+                            }
+                        </AnimatePresence>
+                    </motion.div>
+                    <div className={` ${searchBarVisible ? 'opacity-0 pointer-events-none' : 'cursor-default'} transition-opacity duration-200 ease-in-out`}>O firmie</div>
+                    <div className={` ${searchBarVisible ? 'opacity-0 pointer-events-none' : 'cursor-default'} transition-opacity duration-200 ease-in-out`}>Realizacje</div>
+                    <div className={` ${searchBarVisible ? 'opacity-0 pointer-events-none' : 'cursor-default'} transition-opacity duration-200 ease-in-out`}>Kontakt</div>
+                    <div className={` ${searchBarVisible ? 'opacity-0 pointer-events-none' : 'cursor-pointer'} transition-opacity duration-200 ease-in-out`}  onClick={() => setSearchBarVisible(prev => !prev)}><Search /></div>
+                    <AnimatePresence>
+                        {searchBarVisible && 
+                            <motion.input className="absolute top-1/2  -translate-y-1/2 right-0 bg-beige rounded-full h-10  p-3 focus:outline-none "
+                            autoFocus
+                                initial={{width: '0', opacity: 0}}
+                                animate={{width: '17.5rem', opacity: 1}}
+                                exit={{width: '0', opacity: 0}}
+                                transition={{ duration: 0.3}}
+                            />
+                        }
+                    </AnimatePresence>
                 </div>
             </div>
             <div></div>
