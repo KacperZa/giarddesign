696172bf60bd94e02470d6766656809fc97d46a8
KacperZa <lacper.zajac765@gmail.com>
2026-07-24 21:11:25 +0200

    style: Poprawiono rozmieszczenie elementów, odstępy między znakami, wysokości linii

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,16 +1,13 @@
-
 const Instagram = () => {
   return (
     <>
         <div className='w-full h-fit grid grid-cols-12 gap-x-16 py-15 bg-grey px-22'>
-            <div className="col-span-10 col-start-2  bg-green px-27.5 py-30 flex flex-row">
-                <div className=" col-span-10 col-start-2 grid grid-cols-10 gap-x-16 justify-start">
-                        <div className="col-span-6">
-                            <p className="font-montserrat text-4-5xl leading-[120%] text-grey">Zostańmy w kontakcie! Znajdziesz nas na  <span className="italic font-inter font-medium">Instagramie.</span> </p>
-                        </div>
-                        <div className="col-span-2 flex flex-col gap-6 col-start-8">
-                            <p className="leading-[150%] text-white">Śledź nasze najnowsze realizacje!</p>
-                            <button className="bg-grey pl-6 pr-6 pt-2.5 pb-3 text-green rounded-full font-inter text-base">Instagram</button>
+            <div className="col-span-10 col-start-2 bg-green px-27.5 py-30 text-grey flex items-center">
+                <div className="grid grid-cols-11 gap-x-16 items-end">
+                    <p className="col-span-7 col-start-2 text-4-5xl leading-[120%] font-montserrat tracking-[-2px]">Zostańmy w kontakcie! Znajdziesz nas na <span className="italic font-medium font-inter tracking-[1px]">Instagramie</span>.</p>
+                    <div className=" col-span-2 flex flex-col gap-6 font-inter leading-[150%]">
+                        <p className="font-inter text-base">Śledź nasze najnowsze realizacje!</p>
+                        <button className=" rounded-full pl-6 pr-6 pb-3 pt-2.5 bg-grey text-green font-inter cursor-pointer">Instagram</button>
                     </div>
                 </div>
             </div>
